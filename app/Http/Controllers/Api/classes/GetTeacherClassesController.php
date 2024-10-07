659a45b07b96101e2fc14e959d7ebe92d7303056
abeerzoher <aberzoher22@gmail.com>
2024-10-07 23:52:56 +0300

    refix code in get teacher classes

--- app/Http/Controllers/Api/classes/GetTeacherClassesController.php
+++ app/Http/Controllers/Api/classes/GetTeacherClassesController.php
@@ -6,8 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\SemesterUser;
 use App\Models\Classes\StudyClass;
-use App\Models\Subjects\Section;
-use App\Models\Subjects\Subject;
 
 class GetTeacherClassesController extends Controller
 {
@@ -17,28 +15,18 @@ class GetTeacherClassesController extends Controller
     //الحصول على الصفوف و الشعب التي يدرسها معلم ضمن الفصل الدراسي
     public function __invoke(Request $request, SemesterUser $semesterUser)
     {
-        if ($request->section_id)
-            $classesAndClassroom = StudyClass::whereHas('assignmentTeachers', function ($query) use ($semesterUser, $request) {
+        $classesAndClassroom = StudyClass::whereHas('assignmentTeachers', function ($query) use ($semesterUser, $request) {
+            $query->where('section_id', $request->section_id)
+                ->orWhere('subject_id', $request->subject_id)
+                ->where('semester_user_id', $semesterUser->id);
+        })->with(['classrooms' =>  function ($query) use ($semesterUser, $request) {
+            $query->whereHas('assignmentTeachers', function ($query) use ($semesterUser, $request) {
                 $query->where('section_id', $request->section_id)
+                    ->orWhere('subject_id', $request->subject_id)
                     ->where('semester_user_id', $semesterUser->id);
-            })->with(['classrooms' =>  function ($query) use ($semesterUser, $request) {
-                $query->whereHas('assignmentTeachers', function ($query) use ($semesterUser, $request) {
-                    $query->where('section_id', $request->section_id)
-                        ->where('semester_user_id', $semesterUser->id);
-                });
-            }])->get();
+            });
+        }])->get();
 
-        else if ($request->subject_id)
-            $classesAndClassroom = StudyClass::whereHas('assignmentTeachers', function ($query) use ($semesterUser, $request) {
-                $query->where('subject_id', $request->subject_id)
-                    ->where('semester_user_id', $semesterUser->id);
-            })->with(['classrooms' =>  function ($query) use ($semesterUser, $request) {
-                $query->whereHas('assignmentTeachers', function ($query) use ($semesterUser, $request) {
-                    $query->where('subject_id', $request->subject_id)
-                        ->where('semester_user_id', $semesterUser->id);
-                });
-            }])->get();
-        else $classesAndClassroom = [];
         return $this->okResponse($classesAndClassroom, 'classes and classroom retrived succsessfully');
     }
 }
